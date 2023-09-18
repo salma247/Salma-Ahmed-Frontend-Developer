7f4fba4155678a976333b1d798150a8d33d8d968
salma247 <salmaelhamshary1@gmail.com>
2023-09-18 13:24:50 +0300

    feat: add navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineMenuAlt1, HiOutlineX } from "react-icons/hi";
+import logo from "../assets/logo.png";
+
+export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="bg-gray-800 rounded">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+                <HiOutlineMenuAlt1 className={`${!isOpen ? "block" : "hidden"} h-6 w-6`} />
+                <HiOutlineX className={`${isOpen ? "block" : "hidden"} h-6 w-6`} />
+            </button>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+                <img className="block h-8 w-auto" src={logo} alt="Workflow" />
+             <h1 className="text-white">SpaceX</h1>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Home
+                </Link>
+                <Link to="/rockets" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Rockets
+                </Link>
+                <Link to="/capsules" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Capsules
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${isOpen ? "block" : "hidden"} sm:hidden`}
+        id="mobile-menu"
+      >
+        <div className="space-y-1 px-2 pb-3 pt-2">
+            <Link to="/" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
+                Home
+            </Link>
+            <Link to="/rockets" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                Rockets
+            </Link>
+            <Link to="/capsules" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                Capsules
+            </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
