960a57ce427a5d1ab6b852b84ddc6ed0d5ef0d3a
salma247 <salmaelhamshary1@gmail.com>
2023-09-18 18:14:37 +0300

    fix: data types

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,39 +1,7 @@
 /// <reference types="vite/client" />
-interface Rocket{
-    type: string;
-    name: string;
-    id: string;
-    active: boolean;
-    cost_per_launch: number;
-    success_rate_pct: number;
-    first_flight: string;
-    country: string;
-    company: string;
-    height: {
-        meters: number;
-        feet: number;
-    };
-    diameter: {
-        meters: number;
-        feet: number;
-    };
-    mass: {
-        kg: number;
-        lb: number;
-    };
-    payload_weights: [
-        {
-            id: string;
-            name: string;
-            kg: number;
-            lb: number;
-        }
-    ];
-    stages: number;
-    wikipedia: string;
-}
 
 interface Capsule{
+    launches: any;
     id: string;
     reuse_count: number;
     water_landings: number;
