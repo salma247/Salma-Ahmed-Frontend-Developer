cca097abbe08e4be790f7538f1571ff6a568ae03
salma247 <salmaelhamshary1@gmail.com>
2023-09-20 18:54:00 +0300

    chore: remove unused data

--- frontend/src/services/paginateData.ts
+++
@@ -1,6 +0,0 @@
-export function paginateData<T>(data: T[], page: number, limit: number): T[] {
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  return data.slice(startIndex, endIndex);
-}
