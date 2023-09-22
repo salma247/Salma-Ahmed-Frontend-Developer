e1d61a3cac27511f522e9dd1f0257f10072d837f
salma247 <salmaelhamshary1@gmail.com>
2023-09-22 19:56:53 +0300

    refactor: useMutation

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { login } from "../services/api";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 
 export function Login() {
   const navigate = useNavigate();
@@ -8,75 +9,88 @@ export function Login() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    try {
-      await login(username, password);
+  const mutation = useMutation(() => login(username, password), {
+    onSuccess: () => {
       navigate("/");
-    } catch (error) {
+    },
+    onError: () => {
       setError(true);
-    }
+    },
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    mutation.mutate();
   };
 
   return (
-    <div className="flex justify-center items-center"
-    style={{
+    <div
+      className="flex items-center justify-center"
+      style={{
         height: "calc(100vh - 150px)",
-    }}>
-        <form
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8"
-            style={{
-                width: "90%",
-                maxWidth: "400px",
-            }}
-            onSubmit={handleSubmit}
-        >
-            <div className="mb-4">
-                <small className=" text-gray-400 text-sm ">
-                    (username and password are both <strong>admin</strong>)
-                </small>
-            </div>
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                    Username
-                </label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="username"
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                />
-            Ss</div>
-            <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                    Password
-                </label>
-                <input
-                    className={`shadow appearance-none border ${
-                        error ? "border-red-500" : ""
-                    } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
-                    id="password"
-                    type="password"
-                    placeholder="******************"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-                {error && (
-                    <p className="text-red-500 text-xs italic">
-                        Please choose a password.
-                    </p>
-                )}
-            </div>
-            <div className="flex items-center justify-between">
-                <button
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="submit"
-                >
-                    Sign In
-                </button>
-            </div>
+      }}
+    >
+      <form
+        className="rounded bg-white px-8 pb-8 pt-6 shadow-md"
+        style={{
+          width: "90%",
+          maxWidth: "400px",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <small className=" text-sm text-gray-400 ">
+            (username and password are both <strong>admin</strong>)
+          </small>
+        </div>
+        <div className="mb-4">
+          <label
+            className="mb-2 block text-sm font-bold text-gray-700"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          Ss
+        </div>
+        <div className="mb-6">
+          <label
+            className="mb-2 block text-sm font-bold text-gray-700"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className={`appearance-none border shadow ${
+              error ? "border-red-500" : ""
+            } focus:shadow-outline mb-3 w-full rounded px-3 py-2 leading-tight text-gray-700 focus:outline-none`}
+            id="password"
+            type="password"
+            placeholder="******************"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          {error && (
+            <p className="text-xs italic text-red-500">
+              Please choose a password.
+            </p>
+          )}
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600 focus:outline-none"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
       </form>
     </div>
   );
