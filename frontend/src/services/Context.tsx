9c8799c3c691d732eebb7d85791e3870a5934deb
salma247 <salmaelhamshary1@gmail.com>
2023-09-22 19:23:40 +0300

    fix: unused context data

--- frontend/src/services/Context.tsx
+++ frontend/src/services/Context.tsx
@@ -1,10 +1,6 @@
 import { createContext, useState, ReactNode } from "react";
 
 type ContextProps = {
-  response: CapsuleResponse;
-  setResponse: (response: CapsuleResponse) => void;
-  data : Capsule[];
-  setData: (data: Capsule[]) => void;
   page: number;
   setPage: (page: number) => void;
   pages: number;
@@ -18,13 +14,11 @@ type Props = {
 export const Context = createContext({} as ContextProps);
 
 export function ContextProvider({ children }: Props) {
-  const [data, setData] = useState<Capsule[]>([]);
   const [page, setPage] = useState(1);
   const [pages, setPages] = useState(1);
-  const [response, setResponse] = useState<CapsuleResponse>({} as CapsuleResponse);
 
   return (
-    <Context.Provider value={{ data, setData, page, setPage, pages, setPages, response, setResponse }}>
+    <Context.Provider value={{ page, setPage, pages, setPages }}>
       {children}
     </Context.Provider>
   );
