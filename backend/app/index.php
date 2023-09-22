4eee0e4acad57b8e1361c19eb30010abe0d01c5e
salma247 <salmaelhamshary1@gmail.com>
2023-09-22 13:41:36 +0300

    fix: add auth header

--- backend/app/index.php
+++ backend/app/index.php
@@ -2,16 +2,14 @@
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Allow-Headers: authorization, Content-Type');
     header('Access-Control-Max-Age: 1728000');
-    header('Content-Length: 0');
     header('Content-Type: text/plain');
     die();
 }
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-
 
 require 'Controller/ApiController.php';
 require 'Controller/LoginController.php';
@@ -29,6 +27,10 @@ $uri = explode('/', $uri);
 $uri = array_filter($uri);
 $uri = array_slice($uri, 1);
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    
+}
+
 if (count($uri) === 1 && $uri[0] === 'login') {
     $loginController->handleRequest();
 } elseif (count($uri) === 1 && $uri[0] === 'search') {
@@ -45,4 +47,3 @@ if (count($uri) === 1 && $uri[0] === 'login') {
     http_response_code(404);
     echo 'Not found';
 }
-
